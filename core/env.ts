325c418cda1ef1a8230f2eb5e9ca00932d6c8e16
MouadxDev <mouad.aitnaim@gmail.com>
2024-10-31 10:10:42 +0100

    Update env.ts ttxx04

--- core/env.ts
+++ core/env.ts
@@ -2,8 +2,8 @@ const ENV = {
     "VITE_BACKEND_API_old":"https://api-cd.clickdoc.ma/api",
     "VITE_BACKEND_old":"https://api-cd.clickdoc.ma",
 
-    "VITE_BACKEND_API":"https://40a5-197-145-182-31.ngrok-free.app/api",
-    "VITE_BACKEND":"https://40a5-197-145-182-31.ngrok-free.app",
+    "VITE_BACKEND_API":"https://5917-197-145-182-31.ngrok-free.app/api",
+    "VITE_BACKEND":"https://5917-197-145-182-31.ngrok-free.app",
     
     "VITE_BACKEND_URL_AUTH":"/auth",
     "VITE_BACKEND_URL_INITIATE":"/initiate",
